9e66f18dbbc28f4c4bb83d8bebb98067c1d3ee2b
Matroskin94 <anton1994anton@mail.ru>
2018-02-01 18:04:43 +0300

    First initial comit: project on React with webpack and webpack-dev-server, with customized ESLint

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+    <div className='root-div'>
+        <h1>Hello react</h1>
+
+    </div>,
+    document.getElementById('root')
+);
