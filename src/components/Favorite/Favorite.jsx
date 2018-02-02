7927d0d9e6d08b0d9a6629eefe83207ac195c4c2
Matroskin94 <anton1994anton@mail.ru>
2018-02-02 14:12:30 +0300

    Deleted node_modules

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 // import { Instruction } from './Instruction';
 import Header from './Header.jsx';
 import FavItem from './FavItem.jsx';
