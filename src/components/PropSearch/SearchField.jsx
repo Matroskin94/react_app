fd0069debc91fdcba22eec0da58b149d8dda8957
Matroskin94 <anton1994anton@mail.ru>
2018-02-15 19:58:20 +0300

    Ready for merge

--- src/components/PropSearch/SearchField.jsx
+++ src/components/PropSearch/SearchField.jsx
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { searchAction } from '../../actions/SearchActions';
+import { searchAction, searchLocationsAction } from '../../actions/SearchActions';
+import goDetailsAction from '../../actions/DetailsActions';
 import ResultLocations from './ResultLocations.jsx';
 import ResultQueries from './ResultQueries.jsx';
 
 class Searchfield extends Component {
-    static propTypes = {
-        setNewQuery: PropTypes.func,
-        queryRessults: PropTypes.array,
-        queries: PropTypes.array
-    };
-
-    static defaultProps = {
-        setNewQuery: '',
-        queryRessults: [],
-        queries: []
-    };
-
     state = {
         inputValue: ''
     };
 
     handleSearchClick = () => {
         this.props.setNewQuery(this.state.inputValue);
+    }
+
+    handleLocationClick = () => {
+        this.props.searchLocations(this.state.inputValue);
+    }
+
+    handleItemClick = item => {
+        this.props.setActiveItem(item);
     }
 
     handleInputChange = event => this.setState({ inputValue: event.target.value });
@@ -35,19 +32,36 @@ class Searchfield extends Component {
                 <button onClick={this.handleSearchClick}>Go</button>
                 <button onClick={this.handleLocationClick}>My Location </button>
                 {this.props.queryRessults.length ?
-                    <ResultLocations results={this.props.queryRessults} /> :
+                    <ResultLocations handleItemClick={this.handleItemClick} results={this.props.queryRessults} /> :
                     <ResultQueries results={this.props.queries} />}
             </div>
         );
     }
 }
 
+Searchfield.propTypes = {
+    setNewQuery: PropTypes.func,
+    searchLocations: PropTypes.func,
+    setActiveItem: PropTypes.func,
+    queryRessults: PropTypes.array,
+    queries: PropTypes.array
+};
+
+Searchfield.defaultProps = {
+    setNewQuery: '',
+    searchLocations: '',
+    setActiveItem: '',
+    queryRessults: [],
+    queries: []
+};
+
 function mapStateToProps(state) {
     return {
         queries: state.searchReducer.queries,
         locations: state.searchReducer.locations,
         queryRessults: state.searchReducer.queryRessults,
-        searchWord: state.searchReducer.searchWord
+        searchWord: state.searchReducer.searchWord,
+        activeItem: state.detailsReducer.activeItem
     };
 }
 
@@ -55,7 +69,14 @@ function mapDispatchToProps(dispatch) {
     return {
         setNewQuery: text => {
             dispatch(searchAction(text));
+        },
+        searchLocations: text => {
+            dispatch(searchLocationsAction(dispatch)('leeds'));
+        },
+        setActiveItem: item => {
+            dispatch(goDetailsAction(item));
         }
+
     };
 }
 
